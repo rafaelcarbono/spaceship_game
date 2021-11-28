79ea9636ed650ca0590c73a120c5708b23044405
rafaelcarbono <rafael.oliveiradesena@outlook.com>
2021-11-28 13:12:27 -0300

    Refinando código css e adicionando movimento do fundo ao js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,18 +1,27 @@
 let instruction = document.getElementById("instruction");
 let game = document.getElementById("game");
-let beggin = document.getElementById("start");
-let enemy = document.createElement("div");;
-let spaceship = document.createElement("div");;
+let beggin = document.getElementById("beggin");
+let enemy = document.createElement("div");
+let spaceship = document.createElement("div");
+let loop = {};
+let y = 0;
 
 function start(){
+
     instruction.classList.add("hidden")
     game.classList.remove("hidden");
     game.appendChild(spaceship);
-    spaceship.classList.add("spaceship");
     game.appendChild(enemy);
+    spaceship.classList.add("spaceship");
     enemy.classList.add("enemy");
-    console.log(game);
 
+    loop.timer = setInterval(changeBack,100);
+
+    function changeBack(){
+        y+=1;
+        backposition = parseInt(window.getComputedStyle(game).backgroundPositionY);
+        game.style.cssText = 'background-position-y:' + y + 'px';
+    }
 
 }
 
