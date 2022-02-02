19072081b6b62679cbba190edf95b0d49e717d5e
rafaelcarbono <rafael.oliveiradesena@outlook.com>
2022-02-02 09:55:28 -0300

    adicionando movimentos na nave

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,6 +5,49 @@ let enemy = document.createElement("div");
 let spaceship = document.createElement("div");
 let loop = {};
 let y = 0;
+      
+
+
+function movePlayer(){
+
+    document.body.addEventListener('keydown', function (e) {
+        let code = e.code;
+        let move = 5;
+        let mover;
+
+        console.log(spaceship.getBoundingClientRect().top);
+    
+        if(code == 'ArrowLeft'){
+            mover = parseInt(window.getComputedStyle(spaceship).left) - move;
+            spaceship.style.cssText = 'left:' + mover + 'px';
+            console.log('ArrowLeft');
+        }
+
+        /* if(code == 'ArrowUp'){
+            mover = parseInt(window.getComputedStyle(spaceship).top) - move;
+            spaceship.style.cssText = 'top:' + mover + 'px';
+            console.log('ArrowUp');
+        }*/
+
+        if(code == 'ArrowRight'){
+            mover = parseInt(window.getComputedStyle(spaceship).left) + move;
+            spaceship.style.cssText = 'left:' + mover + 'px';
+            console.log('ArrowRight');
+        }
+        /*
+        if(code == 'ArrowDown'){
+            mover = parseInt(window.getComputedStyle(spaceship).top) + move;
+            spaceship.style.cssText = 'top:' + mover + 'px';
+            console.log('ArrowDown');
+        }*/
+
+        if(code == 'Space'){
+            console.log('Space');
+        }
+
+      });
+    
+}
 
 function start(){
 
@@ -22,6 +65,8 @@ function start(){
         backposition = parseInt(window.getComputedStyle(game).backgroundPositionY);
         game.style.cssText = 'background-position-y:' + y + 'px';
     }
+
+    movePlayer();
 
 }
 
